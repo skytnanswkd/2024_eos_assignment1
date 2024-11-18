6054eaa833db4ed7e2c87701ee931164bcc63098
skytnanswkd <dongho0447@gmail.com>
2024-11-18 11:20:47 +0900

    commit message]

---
+++ lib/widgets/add_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class AddButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  const AddButton({Key? key, required this.onPressed}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Icon(
+        Icons.add,
+        size: 30,
+        color: Colors.white,
+      ),
+      style: ElevatedButton.styleFrom(
+          elevation: 10,
+          backgroundColor: Color(0xFFA4C639).withOpacity(0.3),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8)),
+          padding: EdgeInsets.all(0),
+          minimumSize: Size(50, 50)),
+    );
+  }
+
+}
